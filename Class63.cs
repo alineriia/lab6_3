6d0d5d6fff0a3049b03e7894168b411bf5b84430
Аліна Шелест <Al.Shelest@ssu.edu.ua>
2020-04-01 14:29:00 +0300

    Добавьте файлы проекта.

---
+++ Class63.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_3
+{
+    class Square: IComparable
+    {
+        protected float a;
+        public Square(int b)
+        {
+            a = b;
+        }
+        public double Perimeter()
+        {
+            return 4 * a;
+        }
+        public double Area()
+        {
+            return a * a;
+        }
+        public void Show()
+        {
+            Console.WriteLine("a = " + a);
+        }
+        public int CompareTo(object obj)
+        {
+            Square b = (Square) obj; //перетворимо до типу DemoPoint
+                                           //визначаємо критерії порівняння поточного об'єкта з параметром в залежності від віддаленості точки від початку
+// координат
+ if (this.Perimeter() < b.Perimeter()) return -1;
+ else if (this.Perimeter() > b.Perimeter()) return 1;
+ else return 0;
+
+        }
+    }
+    
+}
---
+++ Main63.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_3
+{
+    
+    class Main63
+    {
+        static void Main(string[] args)
+        {
+            Square[] a = new Square[3];
+            a[0] = new Square(2);
+            a[1] = new Square(5);
+            a[2] = new Square(4);
+            Console.WriteLine("\n====== SORTING BY PERIMETER ====== \n");//сортування за середнім балом
+            Array.Sort(a);
+            foreach (Square s in a)
+            {
+                Console.WriteLine("{0}", s.Perimeter());
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
